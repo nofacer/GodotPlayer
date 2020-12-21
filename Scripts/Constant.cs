25c765b6cf6e4890571f2f361a6488612c2fc780
九日木彡 <nofacer@outlook.com>
2020-12-21 22:02:15 +0800

    add a constant class

---
+++ Scripts/Constant.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public static class Constant
+{
+    public static int PLAYER_RUN_SPEED = 60;
+}
--- Scripts/States/RunState.cs
+++ Scripts/States/RunState.cs
@@ -15,6 +15,6 @@ public class RunState : PlayerState
         animatedSprite.FlipH = (command.name == "walk_left") ? true : false;
         int flip = (command.name == "walk_left") ? -1 : 1;
         animatedSprite.Play("run");
-        character.MoveAndSlide(new Vector2(flip*40, 0), new Vector2(0, -1));
+        character.MoveAndSlide(new Vector2(flip * Constant.PLAYER_RUN_SPEED, 0), new Vector2(0, -1));
     }
 }
