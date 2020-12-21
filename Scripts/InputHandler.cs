1ee45e383e798dc845643255d8d5306ed06d34f6
九日木彡 <nofacer@outlook.com>
2020-12-21 21:53:55 +0800

    Clean up unused  commands

--- Scripts/InputHandler.cs
+++ Scripts/InputHandler.cs
@@ -3,14 +3,12 @@ using System;
 
 public class InputHandler : Node
 {
-    private WalkRightCommand walkRightCommand = new WalkRightCommand("run", "walk_right");
-    private WalkLeftCommand walkLeftCommand = new WalkLeftCommand("run", "walk_left");
-    public InputHandler() { }
+
 
     public Command handleInput()
     {
-        if (Input.IsActionPressed("ui_right")) { return walkRightCommand; }
-        if (Input.IsActionPressed("ui_left")) { return walkLeftCommand; }
+        if (Input.IsActionPressed("ui_right")) { return new Command("run", "walk_right"); }
+        if (Input.IsActionPressed("ui_left")) { return new Command("run", "walk_left"); }
         return null;
     }
 }
